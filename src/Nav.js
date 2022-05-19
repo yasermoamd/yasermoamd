994ad54b998d1d32b7b883156c54aa381e6bafaa
yasermoamd <yasermo7am3d@gmail.com>
2022-05-19 12:45:08 -0700

    project setup

---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+
+import leftIcon from './assets/icon.png'
+
+export const Nav = () => {
+    return (
+         <div>
+          <div className="absolute top-0 right-0 h-12 w-18 p-4">
+            <button className="js-change-theme focus:outline-none">🌙</button>
+          </div>
+         </div>
+    )
+}
